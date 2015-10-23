39ec4c6ed48b8ea60169fc7914ccb1593457e6c8
AnselHall <1206313836@qq.com>
2015-10-23 17:50:08 +0800

    1

---
+++ app/src/main/java/com/tools/exchange/bean/ItemDetail.java
@@ -0,0 +1,24 @@
+package com.tools.exchange.bean;
+
+/**
+ * Created by user on 2015/10/23.
+ */
+public class ItemDetail {
+
+    public String code;/*货币代码*/
+
+    public String name;/*货币名称*/
+
+    public String fBuyPri;/*现钞买入价*/
+
+    public String mBuyPri;/*现汇买入价*/
+
+    public String fSellPri;/*现钞卖出价*/
+
+    public String mSellPri;/*现汇卖出价*/
+
+    public String bankConversionPri;/*中行折算价*/
+
+    public String updateTime;/*发布日期和事件*/
+
+}
